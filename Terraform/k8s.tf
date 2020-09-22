9b885e028445685b6b86c4c774d5af84cea1db1a
Neelesh Jain <Neelesh_Jain@hotmail.com>
2020-09-22 15:38:43 +0530

    ddd

--- Terraform/k8s.tf
+++ Terraform/k8s.tf
@@ -34,18 +34,18 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     resource_group_name = azurerm_resource_group.k8s.name
     dns_prefix          = var.dns_prefix
     
-    linux_profile {
-        admin_username = "ubuntu"
+    # linux_profile {
+    #     admin_username = "ubuntu"
 
-        # ssh_key {
-        #     key_data = file(var.ssh_public_key)
-        # }
-    }
+    #     # ssh_key {
+    #     #     key_data = file(var.ssh_public_key)
+    #     # }
+    # }
 
     default_node_pool {
         name            = "agentpool"
         node_count      = var.agent_count
-        vm_size         = "Standard_B2s"
+        vm_size         = "Standard_B2s"        
     }
 
     # service_principal {
@@ -62,7 +62,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
     network_profile {
     load_balancer_sku = "Standard"
-    network_plugin = "kubenet"
+    network_plugin = "kubenet"    
     }
 
     tags = {
